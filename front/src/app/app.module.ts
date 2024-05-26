561ae2e736e4e4c37ffad661aa3c5a3a20f4eb69
SofiaBritps <sofiibritos01@gmail.com>
2024-05-25 21:53:38 -0300

    navbar para movil terminado

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,13 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutComponent } from './shared/layout/layout.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatSidenavModule} from  '@angular/material/sidenav';
+import {MatButtonModule} from '@angular/material/button'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
